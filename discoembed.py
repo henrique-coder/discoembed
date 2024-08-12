de103ceac67a876be65d0e08b0f7da047c9a3bb0
Henrique <henriquemoreira10fk@gmail.com>
2024-08-12 18:08:05 -0300

    Add source code (api)

---
+++ discoembed.py
@@ -0,0 +1,96 @@
+# Built-in modules
+from http import HTTPStatus
+from pathlib import Path
+from typing import *
+
+# Third-party modules
+from flask import Flask, request, render_template_string
+from flask_caching import Cache
+from flask_compress import Compress
+from flask_cors import CORS
+from flask_talisman import Talisman
+from flask_wtf.csrf import CSRFProtect
+from orjson import loads as orjson_loads
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+# Local modules
+from static.data.functions import CacheTools, is_valid_url
+from static.data.logger import logger
+
+
+# Setup Flask application and debugging mode
+app = Flask(__name__)
+
+# Setup Flask cache
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+logger.info(f'Flask cache enabled. Cache type: {cache.config['CACHE_TYPE']}')
+
+# Setup Talisman for security headers
+talisman = Talisman(app, content_security_policy={'default-src': ["'self'", 'https://cdnjs.cloudflare.com'], 'style-src': ["'self'", "'unsafe-inline'", 'https://cdnjs.cloudflare.com'], 'script-src': ["'self'", 'https://cdnjs.cloudflare.com']})
+logger.info('Talisman security headers enabled')
+
+# Setup Flask CSRF protection
+CSRFProtect(app)
+
+# Setup Flask Compress for GZIP compression
+Compress(app)
+logger.info('Response compression enabled')
+
+# Setup Flask CORS
+CORS(app, resources={r'*': {'origins': '*'}})
+logger.info('CORS enabled')
+
+# Fix for Flask behind a reverse proxy
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
+
+
+# Function to create an embed HTML
+def make_embed_html(url: str, cover: str, width: int = None, height: int = None) -> str:
+    return f'<!DOCTYPE html><html lang="en"><head><meta property="og:video:url" content="{url}"><meta property="og:image" content="{cover}"><meta property="og:video:width" content="{width if width else ""}"><meta property="og:video:height" content="{height if height else ""}"><meta property="og:type" content="video.other"></head></html>'
+
+# Setup API routes
+@app.route(f'/embed', methods=['GET'])
+@cache.cached(timeout=1, make_cache_key=CacheTools.gen_cache_key)
+def embed() -> Tuple[render_template_string, HTTPStatus]:
+    # Get parameters from the URL
+    url = request.args.get('url')
+    cover = request.args.get('cover')
+    width = request.args.get('width')
+    height = request.args.get('height')
+
+    # Check if the URL is valid
+    if not url:
+        return render_template_string(make_embed_html('https://i.imgur.com/k9FrlEe.mp4', 'https://i.imgur.com/bUNtIgQ.png', 1280, 720)), HTTPStatus.BAD_REQUEST
+    elif not is_valid_url(url, online_check=True):
+        return render_template_string(make_embed_html('https://i.imgur.com/bMGv6H5.mp4', 'https://i.imgur.com/kTj9dnk.png', 1280, 720)), HTTPStatus.BAD_REQUEST
+
+    # Check if the cover is valid
+    if not cover:
+        cover = 'https://i.imgur.com/qFu40E1.png'
+    elif not is_valid_url(cover, online_check=True):
+        return render_template_string(make_embed_html(url, 'https://i.imgur.com/Cl6kMsz.png', width, height)), HTTPStatus.BAD_REQUEST
+
+    # Check if the width and height are valid
+    try:
+        width = int(width)
+        height = int(height)
+    except ValueError:
+        width, height = '', ''
+
+    # Return the embed HTML
+    return render_template_string(make_embed_html(url, cover, width, height)), HTTPStatus.OK
+
+
+if __name__ == '__main__':
+    # Load the configuration file
+    current_path = Path(__file__).parent
+    config_path = Path(current_path, 'config.json')
+    config = orjson_loads(config_path.read_text())
+
+    # Setting up Flask default configuration
+    app.static_folder = Path(current_path, config['flask']['staticFolder'])
+    app.template_folder = Path(current_path, config['flask']['templateFolder'])
+
+    # Run the web server with the specified configuration
+    logger.info(f'Starting web server at {config['flask']['host']}:{config['flask']['port']}')
+    app.run(debug=True, host=config['flask']['host'], port=config['flask']['port'], threaded=config['flask']['threadedServer'])
---
+++ static/data/functions.py
@@ -0,0 +1,55 @@
+# Built-in modules
+from typing import Optional
+
+# Third-party modules
+from faker import Faker
+from flask import request as flask_request
+from httpx import head, HTTPError
+from validators import url as is_url, ValidationError
+
+
+# Constants
+fake = Faker()
+
+
+class CacheTools:
+    """
+    A class for cache tools.
+    """
+
+    @staticmethod
+    def gen_cache_key(*args, **kwargs) -> str:
+        """
+        Generate a cache key for the current request.
+        :param args: The arguments for the current request.
+        :param kwargs: The keyword arguments for the current request.
+        :return: A cache key for the current request.
+        """
+
+        return flask_request.url
+
+
+def is_valid_url(url: str, online_check: bool = False) -> Optional[bool]:
+    """
+    Check if a URL is valid and reachable online.
+    :param url: The URL to check.
+    :param online_check: If True, check if the URL is reachable online.
+    :return: True if the URL is valid, False if the URL is invalid, and None if the URL is unreachable online.
+    """
+
+    try:
+        bool_value = bool(is_url(url))
+
+        if not bool_value:
+            return False
+    except ValidationError:
+        return False
+
+    if online_check:
+        try:
+            response = head(url, headers={'User-Agent': fake.user_agent(), 'X-Forwarded-For': fake.ipv4_public()}, follow_redirects=True, timeout=10)
+            return True if response.is_success or response.is_redirect else None
+        except HTTPError:
+            return None
+
+    return bool_value
