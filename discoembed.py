6c815a600fd81a2282e56f15d91c4c9b5cd28481
Henrique <henriquemoreira10fk@gmail.com>
2024-08-12 18:38:03 -0300

    Change cache to 8h

--- discoembed.py
+++ discoembed.py
@@ -50,7 +50,7 @@ def make_embed_html(url: str, cover: str, width: int = None, height: int = None)
 
 # Setup API routes
 @app.route(f'/embed', methods=['GET'])
-@cache.cached(timeout=1, make_cache_key=CacheTools.gen_cache_key)
+@cache.cached(timeout=28800, make_cache_key=CacheTools.gen_cache_key)
 def embed() -> Tuple[render_template_string, HTTPStatus]:
     logger.info(f'GET request received at /embed')
     
