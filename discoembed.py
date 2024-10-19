ac1eea8e5b5e248133084bb607834a4a7aac2a42
Henrique <henriquemoreira10fk@gmail.com>
2024-10-19 08:33:56 -0300

    Removed online check and changed cache timeout to 5s

--- discoembed.py
+++ discoembed.py
@@ -51,7 +51,7 @@ def make_embed_html(url: str, cover: str, width: int = None, height: int = None)
 
 # Setup API routes
 @app.route('/', methods=['GET'])
-@cache.cached(timeout=60, make_cache_key=CacheTools.gen_cache_key)
+@cache.cached(timeout=5, make_cache_key=CacheTools.gen_cache_key)
 def index() -> Tuple[Union[Response, render_template_string], HTTPStatus]:
     logger.info(f'GET request received from ip {request.remote_addr} with user agent {request.user_agent}')
 
@@ -67,13 +67,13 @@ def index() -> Tuple[Union[Response, render_template_string], HTTPStatus]:
     # Check if the URL is valid
     if not url:
         return render_template_string(make_embed_html('https://i.imgur.com/k9FrlEe.mp4', 'https://i.imgur.com/bUNtIgQ.png', 1280, 720)), HTTPStatus.OK
-    elif not is_valid_url(url, online_check=True):
+    elif not is_valid_url(url, online_check=False):
         return render_template_string(make_embed_html('https://i.imgur.com/bMGv6H5.mp4', 'https://i.imgur.com/kTj9dnk.png', 1280, 720)), HTTPStatus.OK
 
     # Check if the cover is valid
     if not cover:
         cover = 'https://i.imgur.com/8ZkUMGK.png'
-    elif not is_valid_url(cover, online_check=True):
+    elif not is_valid_url(cover, online_check=False):
         return render_template_string(make_embed_html(url, 'https://i.imgur.com/Cl6kMsz.png', width, height)), HTTPStatus.OK
 
     # Check if the width and height are valid
